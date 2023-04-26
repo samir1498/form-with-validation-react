c0ab6e06c613dd75800fad1f8d1059afc6f7290e
samir1498 <samirsbx2@gmail.com>
2023-04-26 15:57:24 +0100

    replacing react logo by the text logo in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,10 @@
 import React from "react"
 
-import logo from "../assets/react.svg"
-
 export default function NavBar() {
   return (
     <>
       <div className="navbar">
-        <img src={logo} alt="logo" className="navbar__log" />
+        <p className="navbar__log">Logo</p>
       </div>
     </>
   )
